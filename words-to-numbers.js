0bd3b7033f413412a59bb7555e51b0403955aac0
Darren Cauthon <darren@cauthon.com>
2016-03-23 06:34:44 -0500

    Convert up to 99.

--- words-to-numbers.js
+++ words-to-numbers.js
@@ -12,6 +12,16 @@ wtn.convert = function(input)
             return this.singles.indexOf(split[1]) + 31;
         if (split[0] == 'forty')
             return this.singles.indexOf(split[1]) + 41;
+        if (split[0] == 'fifty')
+            return this.singles.indexOf(split[1]) + 51;
+        if (split[0] == 'sixty')
+            return this.singles.indexOf(split[1]) + 61;
+        if (split[0] == 'seventy')
+            return this.singles.indexOf(split[1]) + 71;
+        if (split[0] == 'eighty')
+            return this.singles.indexOf(split[1]) + 81;
+        if (split[0] == 'ninety')
+            return this.singles.indexOf(split[1]) + 91;
         return this.singles.indexOf(split[1]) + 21;
     }
     return single + 1;
