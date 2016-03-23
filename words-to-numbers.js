6e528b584dc8885b5a0798d14dfe82df6ed77069
Darren Cauthon <darren@cauthon.com>
2016-03-23 06:23:58 -0500

    Handle twenties.

--- words-to-numbers.js
+++ words-to-numbers.js
@@ -4,5 +4,8 @@ wtn.singles = ["one", "two", "three", "four", "five", "six", "seven", "eight", "
 
 wtn.convert = function(input)
 {
-    return this.singles.indexOf(input.toLowerCase()) + 1;
+    var single = this.singles.indexOf(input.toLowerCase());
+    if (single == -1)
+        return this.singles.indexOf(input.split(' ')[1]) + 21;
+    return single + 1;
 }
