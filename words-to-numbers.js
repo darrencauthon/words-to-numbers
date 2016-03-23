55265c92082ffd8dd15ebb4054ce39f8d11d9480
Darren Cauthon <darren@cauthon.com>
2016-03-23 06:36:56 -0500

    Refactor.

--- words-to-numbers.js
+++ words-to-numbers.js
@@ -2,27 +2,25 @@ wtn = {};
 
 wtn.singles = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"];
 
+wtn.tens = {
+               'twenty': 20,
+               'thirty': 30,
+               'forty': 40,
+               'fifty': 50,
+               'sixty': 60,
+               'seventy': 70,
+               'eighty': 80,
+               'ninety': 90
+           };
+
 wtn.convert = function(input)
 {
     var single = this.singles.indexOf(input.toLowerCase());
     if (single == -1)
     {
         var split = input.split(' ');
-        if (split[0] == 'thirty')
-            return this.singles.indexOf(split[1]) + 31;
-        if (split[0] == 'forty')
-            return this.singles.indexOf(split[1]) + 41;
-        if (split[0] == 'fifty')
-            return this.singles.indexOf(split[1]) + 51;
-        if (split[0] == 'sixty')
-            return this.singles.indexOf(split[1]) + 61;
-        if (split[0] == 'seventy')
-            return this.singles.indexOf(split[1]) + 71;
-        if (split[0] == 'eighty')
-            return this.singles.indexOf(split[1]) + 81;
-        if (split[0] == 'ninety')
-            return this.singles.indexOf(split[1]) + 91;
-        return this.singles.indexOf(split[1]) + 21;
+        if (wtn.tens[split[0]] != undefined)
+            return this.singles.indexOf(split[1]) + 1 + wtn.tens[split[0]];
     }
     return single + 1;
 }
