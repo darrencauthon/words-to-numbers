75a5657764f9c718c2701950529ce369e4a0fb62
Darren Cauthon <darren@cauthon.com>
2016-03-23 06:25:45 -0500

    Handle thirties.

--- words-to-numbers.js
+++ words-to-numbers.js
@@ -6,6 +6,11 @@ wtn.convert = function(input)
 {
     var single = this.singles.indexOf(input.toLowerCase());
     if (single == -1)
-        return this.singles.indexOf(input.split(' ')[1]) + 21;
+    {
+        var split = input.split(' ');
+        if (split[0] == 'thirty')
+            return this.singles.indexOf(split[1]) + 31;
+        return this.singles.indexOf(split[1]) + 21;
+    }
     return single + 1;
 }
