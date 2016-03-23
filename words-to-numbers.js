71a57ae18e8591cac22091b965229a8800ce4f1c
Darren Cauthon <darren@cauthon.com>
2016-03-23 06:18:59 -0500

    Handle larger numbers.

--- words-to-numbers.js
+++ words-to-numbers.js
@@ -1,6 +1,6 @@
 wtn = {};
 
-wtn.singles = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+wtn.singles = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"];
 
 wtn.convert = function(input)
 {
