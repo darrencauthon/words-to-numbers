4190a747c53ba8f8b1fb535446e8f00b7c814d40
Darren Cauthon <darren@cauthon.com>
2016-03-31 06:44:54 -0500

    Spacing.

--- words-to-numbers.js
+++ words-to-numbers.js
@@ -2,36 +2,36 @@ wtn = {};
 
 wtn.singles = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"];
 wtn.singles = {
-                  'one': 1,
-                  'two': 2,
-                  'three': 3,
-                  'four': 4,
-                  'five': 5,
-                  'six': 6,
-                  'seven': 7,
-                  'eight': 8,
-                  'nine': 9,
-                  'ten': 10,
-                  'eleven': 11,
-                  'twelve': 12,
-                  'thirteen': 13,
-                  'fourteen': 14,
-                  'fifteen': 15,
-                  'sixteen': 16,
+                  'one':        1,
+                  'two':        2,
+                  'three':      3,
+                  'four':       4,
+                  'five':       5,
+                  'six':        6,
+                  'seven':      7,
+                  'eight':      8,
+                  'nine':       9,
+                  'ten':       10,
+                  'eleven':    11,
+                  'twelve':    12,
+                  'thirteen':  13,
+                  'fourteen':  14,
+                  'fifteen':   15,
+                  'sixteen':   16,
                   'seventeen': 17,
-                  'eighteen': 18,
-                  'nineteen': 19
+                  'eighteen':  18,
+                  'nineteen':  19
               };
 
 wtn.tens = {
-               'twenty': 20,
-               'thirty': 30,
-               'forty': 40,
-               'fifty': 50,
-               'sixty': 60,
+               'twenty':  20,
+               'thirty':  30,
+               'forty':   40,
+               'fifty':   50,
+               'sixty':   60,
                'seventy': 70,
-               'eighty': 80,
-               'ninety': 90
+               'eighty':  80,
+               'ninety':  90
            };
 
 wtn.complicated_numbers = {
